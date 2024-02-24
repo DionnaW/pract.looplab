df321c0a3dff8a690a87ec70194f698dd05d96a6
Dionna <businessladythick@yahoo.com>
2024-02-24 15:19:27 -0500

    i wonder how would i know which numbers exactly

--- ploop.js
+++ ploop.js
@@ -1,14 +1,21 @@
-//Fizz  :(
+//A number divisible by 3 is Fizz  
     for( let i = 1; i <= 100; i++) {
         if(i % 3 ===0) {
             console.log("Fizz");
         }
     }
 
-//Buzz
+//A number divisible by 5 is Buzz
 for( let i = 1; i <= 100; i++) {
     if(i % 5 ===0) {
         console.log("Buzz");
     }
 }  
+
+//Number divisible by 3 and 5 is Fizz Buzz
+function fizzBuzz(number) {
+    if(number % 3 === 0 && number % 5 === 0) {
+        console.log("Fizz Buzz")
+    }
+}
 
