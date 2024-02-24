66769e0409d65844875ca2c04e0b5e7cde3c8ff7
Dionna <businessladythick@yahoo.com>
2024-02-24 15:13:21 -0500

    still just says Buzz no numbers

--- ploop.js
+++ ploop.js
@@ -1,6 +1,14 @@
-//Fizz Buzz :(
+//Fizz  :(
     for( let i = 1; i <= 100; i++) {
         if(i % 3 ===0) {
             console.log("Fizz");
         }
     }
+
+//Buzz
+for( let i = 1; i <= 100; i++) {
+    if(i % 5 ===0) {
+        console.log("Buzz");
+    }
+}  
+
