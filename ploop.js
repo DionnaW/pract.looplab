1c8861c5e0a658f702390b5cdd12539ded90b9ef
Dionna <businessladythick@yahoo.com>
2024-02-26 22:17:05 -0500

    nothing

--- ploop.js
+++ ploop.js
@@ -62,21 +62,21 @@ while( true) {
 
 //Part 3
 //Loopy CVS data
-// const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
 
 //split the string into rows
-// const rows = csvString.split("\n");
+const rows = csvString.split("\n");
 
 //to make sure each row is seperated
-// rows.forEach(row => {
+rows.forEach(row => {
     //splitting the rows into columns
-    // const columns = row.split(",");
+    const columns = row.split(",");
     //run console to log each row of data
-    // console.log(columns);
+    console.log(columns);
     //seeing if this way also runs correctly
     // console.log(cell1, cell2, cell3, cell4);
     //it doesn't
-// })
+})
 
 //trying this now that the one above worked
 // const csvString = "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
